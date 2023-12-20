b777b73ed243731fb2d3bab8ab990f25f3a8d378
MilenaPacheco <86914621+MilenaPacheco@users.noreply.github.com>
2023-12-20 02:18:52 -0500

    Correccion Funciones

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
-import analyzer from './analyzer.js';
+//import analyzer from './analyzer.js';                          
 
-//TODO: escuchar eventos del DOM e invocar  los métodos del objeto `analyzer`
+//TODO: escuchar eventos del DOM e invocar los métodos del objeto `analyzer`
+
+//Event Listener Textarea
+const texto = document.querySelector("textarea")
+texto.addEventListener("input", getText)
+function getText(){
+    let texto2 = document.querySelector(textarea).value
+    document.getElementById("vista").innerHTML = texto2
+}
+
+//Event Listener Botón 
+const boton = document.getElementById("reset-button");
+boton.addEventListener("click", resetMetricas)
+function resetMetricas(){
+    document.querySelector("textarea").value=""; //En el textarea se ingresará el valor nulo
+}
