469a31b51eb07600d071e1ca836849c13ee80a14
MilenaPacheco <86914621+MilenaPacheco@users.noreply.github.com>
2023-12-20 21:15:04 -0500

    correccion Funciones

--- src/analyzer.js
+++ src/analyzer.js
@@ -1,21 +1,63 @@
 const analyzer = {  
   getWordCount: (text) => {
     //TODO: esta función debe retornar el recuento de palabras que se encuentran en el parámetro `text` de tipo `string`.
+    const words = text.trim().split(" ").length;
+    return words
+    //console.log(words)
   },
+
   getCharacterCount: (text) => {
     //TODO: esta función debe retornar el recuento de caracteres que se encuentran en el parámetro `text` de tipo `string`.
+    const characterCount = text
+    //console.log(characterCount.length)
+    return characterCount.length
   },
   getCharacterCountExcludingSpaces: (text) => {
     //TODO: esta función debe retornar el recuento de caracteres excluyendo espacios y signos de puntuación que se encuentran en el parámetro `text` de tipo `string`.
+    const characterExcluSpaces = text.trim().replace(/[^\w\s]|_/g, "").replace(/\s+/g, "").length;
+    return characterExcluSpaces
   },
   getAverageWordLength: (text) => {    
     //TODO: esta función debe retornar la longitud media de palabras que se encuentran en el parámetro `text` de tipo `string`.
+    const words = text.trim().split(" ").length
+    const characterExcluSpaces = text.trim().replace(/\s+/g, "").length;
+    let wordLenght = 0;
+    if (words===0){
+      wordLenght = 0;
+    } else{
+      wordLenght = (parseFloat(characterExcluSpaces)/parseFloat(words));
+    }
+    return Number(wordLenght.toFixed(2))
+    //console.log(wordLenght);
   },
   getNumberCount: (text) => {
-    //TODO: esta función debe retornar cúantos números se encuentran en el parámetro `text` de tipo `string`.
+    const  numberCount = text.match(/\b\d+(\.\d+)?\b/g);
+    if (numberCount){
+      return numberCount.length
+    }
+    else{
+      return 0;
+    }
   },
   getNumberSum: (text) => {
     //TODO: esta función debe retornar la suma de todos los números que se encuentran en el parámetro `text` de tipo `string`.
+    let numberSumTotal=0;
+    const numberSum2 = text.trim().split(" ");
+    //console.log("Number sum 2: "+numberSum2)
+    //console.log(numberSum2)
+    for(let i=0;i<numberSum2.length;i++){
+      if (Number(numberSum2[i])){
+        numberSum2[i]=parseFloat(numberSum2[i]);
+        //console.log(numberSum2)
+       
+        numberSumTotal=parseFloat(numberSum2[i]+numberSumTotal)
+        console.log(numberSumTotal)
+        console.log("Suma Array: "+ numberSumTotal)
+
+      }
+    }
+      
+    return numberSumTotal
   },
 };
 
--- src/index.js
+++ src/index.js
@@ -1,18 +1,40 @@
-//import analyzer from './analyzer.js';                          
+import analyzer from './analyzer.js';                          
 
 //TODO: escuchar eventos del DOM e invocar los métodos del objeto `analyzer`
 
-//Event Listener Textarea
-const texto = document.querySelector("textarea")
-texto.addEventListener("input", getText)
-function getText(){
-    let texto2 = document.querySelector(textarea).value
-    document.getElementById("vista").innerHTML = texto2
-}
+const text = document.querySelector("textarea");
+
+
+text.addEventListener("input", function() {
+  const words = analyzer.getWordCount(text.value);
+  document.querySelector(".wordCount").innerHTML ="Palabras: "+ words
+
+  const characterCount = analyzer.getCharacterCount(text.value);
+  document.querySelector(".caracterCount").innerHTML = "Caracteres: " + characterCount
+
+  const characterExcluSpaces = analyzer.getCharacterCountExcludingSpaces(text.value);
+  document.querySelector(".character-no-spaces").innerHTML="Caracteres Sin Espacio: "+ characterExcluSpaces
+
+  const textWordLenght = analyzer.getAverageWordLength(text.value);
+  document.querySelector(".word-length-average").innerHTML = "Promedio Longitud: " + textWordLenght;
+
+  const  numberCount =analyzer.getNumberCount(text.value);
+  document.querySelector(".number-count").innerHTML = "Números: " + numberCount;
+
+  const numberSumTotal = analyzer.getNumberSum(text.value);
+  document.querySelector(".number-sum").innerHTML = "Suma Números: " + numberSumTotal
+} )
+
 
 //Event Listener Botón 
-const boton = document.getElementById("reset-button");
-boton.addEventListener("click", resetMetricas)
+const boton=document.getElementById("reset-button"); //asignar variable del HTML al JS
+boton.addEventListener("click", resetMetricas) //Al hacer el evento haga esta función
 function resetMetricas(){
-    document.querySelector("textarea").value=""; //En el textarea se ingresará el valor nulo
+  document.querySelector("textarea").value=""; //En el textarea se ingresará el valor nulo
+  document.querySelector(".wordCount").innerHTML ="Palabras: ";
+  document.querySelector(".caracterCount").innerHTML = "Caracteres: ";
+  document.querySelector(".character-no-spaces").innerHTML="Caracteres Sin Espacio: ";
+  document.querySelector(".word-length-average").innerHTML = "Promedio Longitud: ";
+  document.querySelector(".number-count").innerHTML = "Números: ";
+  document.querySelector(".number-sum").innerHTML = "Suma Números: ";
 }
