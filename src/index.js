e4606a5b21f85df1297038c6c28a010682a9c1f0
MilenaPacheco <86914621+MilenaPacheco@users.noreply.github.com>
2023-12-21 23:48:11 -0500

    Correccion etiquetas CSS

--- src/index.js
+++ src/index.js
@@ -7,22 +7,22 @@ const text = document.querySelector("textarea");
 
 text.addEventListener("input", function() {
   const words = analyzer.getWordCount(text.value);
-  document.querySelector(".wordCount").innerHTML ="Palabras: "+ words
+  document.querySelector("li.estilo1:nth-child(1)").innerHTML ="Palabras: "+ words
 
   const characterCount = analyzer.getCharacterCount(text.value);
-  document.querySelector(".caracterCount").innerHTML = "Caracteres: " + characterCount
+  document.querySelector("li.estilo1:nth-child(2)").innerHTML = "Caracteres: " + characterCount
 
   const characterExcluSpaces = analyzer.getCharacterCountExcludingSpaces(text.value);
-  document.querySelector(".character-no-spaces").innerHTML="Caracteres Sin Espacio: "+ characterExcluSpaces
+  document.querySelector("li.estilo1:nth-child(3)").innerHTML="Caracteres Sin Espacio: "+ characterExcluSpaces
 
   const textWordLenght = analyzer.getAverageWordLength(text.value);
-  document.querySelector(".word-length-average").innerHTML = "Promedio Longitud: " + textWordLenght;
+  document.querySelector("li.estilo2:nth-child(4)").innerHTML = "Promedio Longitud: " + textWordLenght;
 
   const  numberCount =analyzer.getNumberCount(text.value);
-  document.querySelector(".number-count").innerHTML = "Números: " + numberCount;
+  document.querySelector("li.estilo2:nth-child(5)").innerHTML = "Números: " + numberCount;
 
   const numberSumTotal = analyzer.getNumberSum(text.value);
-  document.querySelector(".number-sum").innerHTML = "Suma Números: " + numberSumTotal
+  document.querySelector("li.estilo2:nth-child(6)").innerHTML = "Suma Números: " + numberSumTotal
 } )
 
 
@@ -31,10 +31,10 @@ const boton=document.getElementById("reset-button"); //asignar variable del HTML
 boton.addEventListener("click", resetMetricas) //Al hacer el evento haga esta función
 function resetMetricas(){
   document.querySelector("textarea").value=""; //En el textarea se ingresará el valor nulo
-  document.querySelector(".wordCount").innerHTML ="Palabras: ";
-  document.querySelector(".caracterCount").innerHTML = "Caracteres: ";
-  document.querySelector(".character-no-spaces").innerHTML="Caracteres Sin Espacio: ";
-  document.querySelector(".word-length-average").innerHTML = "Promedio Longitud: ";
-  document.querySelector(".number-count").innerHTML = "Números: ";
-  document.querySelector(".number-sum").innerHTML = "Suma Números: ";
+  document.querySelector("li.estilo1:nth-child(1)").innerHTML ="Palabras: ";
+  document.querySelector("li.estilo1:nth-child(2)").innerHTML = "Caracteres: ";
+  document.querySelector("li.estilo1:nth-child(3)").innerHTML="Caracteres Sin Espacio: ";
+  document.querySelector("li.estilo2:nth-child(4)").innerHTML = "Promedio Longitud: ";
+  document.querySelector("li.estilo2:nth-child(5)").innerHTML = "Números: ";
+  document.querySelector("li.estilo2:nth-child(6)").innerHTML = "Suma Números: ";
 }
